95dc2cc98f2d7fb0d088aec3c5b71bb41eb86908
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2025-10-04 22:30:19 +0530

    notification tap and profile page added to homepage

--- Backend/Routes/sellerRoute.js
+++ Backend/Routes/sellerRoute.js
@@ -5,11 +5,15 @@ const Seller = require("../Model/sellerModel");
 
 const SellerController = require("../Controllers/sellerControl");
 
+
 router.get("/", SellerController.getAllSeller);
 router.post("/", SellerController.addSeller);
 router.get("/:tankId", SellerController.getById);
 router.put("/:id", SellerController.updateSeller);
 router.delete("/:id", SellerController.deleteSeller);
+
+// Password change route (by tankId)
+router.put("/:tankId/password", SellerController.changePassword);
 
 
 
